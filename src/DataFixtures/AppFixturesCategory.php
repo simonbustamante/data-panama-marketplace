a7608c9f0ffd18f2deb20a854ab68c6e356da892
Simon Bustamante <simon.bustamante@gmail.com>
2024-05-22 17:16:52 -0500

    append

--- src/DataFixtures/AppFixturesCategory.php
+++ src/DataFixtures/AppFixturesCategory.php
@@ -9,6 +9,7 @@ use App\Repository\StoreRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
+use Ramsey\Uuid\Uuid;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixturesCategory extends Fixture implements DependentFixtureInterface
@@ -28,11 +29,12 @@ class AppFixturesCategory extends Fixture implements DependentFixtureInterface
     {
         $count = 1;
         foreach($this->category_list as $cat)  {
+            $uniqueId = Uuid::uuid4();
             $category = new Category();
             // $category->setId($count);
-            $category->setCategoryId($count++);
-            $category->setName($cat['name']);
-            $category->setDescription("Description " . $cat['name']);
+            $category->setCategoryId($uniqueId->toString());
+            $category->setName($cat['name']." ".$uniqueId->toString());
+            $category->setDescription("Description " . $cat['name']." ".$uniqueId->toString());
             $manager->persist($category);
         }
         $manager->flush();
